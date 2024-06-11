6013c9379e4ce67886d7d6434dc1f909f8806a5b
Alexander Biryukov <74318440+AlexFromNorth@users.noreply.github.com>
2024-06-11 15:50:03 +0300

    create vite build options

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ const world = 'world';
 export function hello(word: string = world): string {
   return `Hello ${world}! `;
 }
+console.log(123)
+console.log(123)
+console.log(2)
+console.log(2)
+console.log(6)
